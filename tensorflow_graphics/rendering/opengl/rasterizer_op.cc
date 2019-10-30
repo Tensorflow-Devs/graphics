1f1c14bb2f93d7fde3bf6382e2a1b4281469c005
julienvalentin <julienvalentin@google.com>
2019-10-30 07:00:22 -0700

    Adding batch support for the rasterizer.
    PiperOrigin-RevId: 277496193

--- tensorflow_graphics/rendering/opengl/rasterizer_op.cc
+++ tensorflow_graphics/rendering/opengl/rasterizer_op.cc
@@ -21,6 +21,9 @@ limitations under the License.
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow/core/lib/core/errors.h"
 #include "tensorflow/core/lib/core/status.h"
 
 REGISTER_OP("Rasterize")
@@ -51,10 +54,7 @@ REGISTER_OP("Rasterize")
             "The number of elements in variable_names, and variable_values.");
 
       // Defines the shape of the output tensor.
-      tensorflow::TensorShape resolution;
-      TF_RETURN_IF_ERROR(c->GetAttr("output_resolution", &resolution));
-      c->set_output(
-          0, c->MakeShape({resolution.dim_size(0), resolution.dim_size(1), 4}));
+      c->set_output(0, c->UnknownShape());
 
       return tensorflow::Status::OK();
     });
@@ -107,21 +107,44 @@ class RasterizeOp : public tensorflow::OpKernel {
   }
 
   void Compute(tensorflow::OpKernelContext* context) override {
+    tensorflow::TensorShape batch_shape;
+    OP_REQUIRES_OK(context, ValidateVariables(context, &batch_shape));
+
+    // Allocate the output images.
+    tensorflow::Tensor* output_image;
+    tensorflow::TensorShape output_image_shape;
+
+    output_image_shape.AppendShape(batch_shape);
+    output_image_shape.AppendShape(output_resolution_);
+    output_image_shape.AddDim(4);
+    OP_REQUIRES_OK(context, context->allocate_output(0, output_image_shape,
+                                                     &output_image));
+
+    // Render.
     std::unique_ptr<RasterizerWithContext> rasterizer;
+    float* image_data = output_image->flat<float>().data();
+    const int64 image_size =
+        output_resolution_.dim_size(0) * output_resolution_.dim_size(1) * 4;
 
     OP_REQUIRES_OK(context, rasterizer_pool_->AcquireResource(&rasterizer));
-    OP_REQUIRES_OK(context, SetVariables(context, rasterizer));
-    OP_REQUIRES_OK(context, RenderImage(context, rasterizer));
+    for (int i = 0; i < batch_shape.num_elements(); ++i) {
+      OP_REQUIRES_OK(context, SetVariables(context, rasterizer, i));
+      OP_REQUIRES_OK(context, RenderImage(context, rasterizer, image_size,
+                                          image_data + i * image_size));
+    }
     OP_REQUIRES_OK(context, rasterizer_pool_->ReturnResource(rasterizer));
   }
 
  private:
   tensorflow::Status SetVariables(
       tensorflow::OpKernelContext* context,
-      std::unique_ptr<RasterizerWithContext>& rasterizer);
+      std::unique_ptr<RasterizerWithContext>& rasterizer, int outer_dim);
   tensorflow::Status RenderImage(
       tensorflow::OpKernelContext* context,
-      std::unique_ptr<RasterizerWithContext>& rasterizer);
+      std::unique_ptr<RasterizerWithContext>& rasterizer, int64 image_size,
+      float* image_data);
+  tensorflow::Status ValidateVariables(tensorflow::OpKernelContext* context,
+                                       tensorflow::TensorShape* batch_shape);
 
   std::unique_ptr<ThreadSafeResourcePool<RasterizerWithContext>>
       rasterizer_pool_;
@@ -132,31 +155,20 @@ class RasterizeOp : public tensorflow::OpKernel {
 
 tensorflow::Status RasterizeOp::RenderImage(
     tensorflow::OpKernelContext* context,
-    std::unique_ptr<RasterizerWithContext>& rasterizer) {
-  tensorflow::Tensor* output_image;
-  tensorflow::TensorShape output_image_shape(
-      {output_resolution_.dim_size(0), output_resolution_.dim_size(1), 4});
-
-  TF_RETURN_IF_ERROR(
-      context->allocate_output(0, output_image_shape, &output_image));
-  auto image_data = output_image->flat<float>();
+    std::unique_ptr<RasterizerWithContext>& rasterizer, const int64 image_size,
+    float* image_data) {
   int num_points = context->input(0).scalar<int>()();
+
   TF_RETURN_IF_ERROR(rasterizer->Render(
-      num_points, absl::MakeSpan(image_data.data(),
-                                 image_data.data() + image_data.size())));
+      num_points, absl::MakeSpan(image_data, image_data + image_size)));
   return tensorflow::Status::OK();
 }
 
 tensorflow::Status RasterizeOp::SetVariables(
     tensorflow::OpKernelContext* context,
-    std::unique_ptr<RasterizerWithContext>& rasterizer) {
+    std::unique_ptr<RasterizerWithContext>& rasterizer, int outer_dim) {
   tensorflow::OpInputList variable_values;
   TF_RETURN_IF_ERROR(context->input_list("variable_values", &variable_values));
-
-  if (variable_names_.size() != variable_values.size()) {
-    return tensorflow::errors::InvalidArgument(
-        "The variable names and values must have the same size.");
-  }
 
   for (int index = 0; index < variable_names_.size(); ++index) {
     const std::string name = variable_names_[index];
@@ -165,22 +177,72 @@ tensorflow::Status RasterizeOp::SetVariables(
     const tensorflow::TensorShape value_shape = value.shape();
     const tensorflow::DataType value_dtype = value.dtype();
 
-    if (kind == "mat" && value_dtype == tensorflow::DT_FLOAT &&
-        value_shape.dims() == 2) {
-      const int num_rows = value_shape.dim_size(0);
-      const int num_cols = value_shape.dim_size(1);
+    if (kind == "mat" && value_dtype == tensorflow::DT_FLOAT) {
+      const int num_rows = value_shape.dim_size(value_shape.dims() - 2);
+      const int num_cols = value_shape.dim_size(value_shape.dims() - 1);
+      const int num_elements = num_rows * num_cols;
+      const auto value_pointer = value.flat<float>().data();
+
       TF_RETURN_IF_ERROR(rasterizer->SetUniformMatrix(
           name, num_cols, num_rows, true,
-          absl::MakeConstSpan(value.flat<float>())));
-    } else if (kind == "buffer" && value_dtype == tensorflow::DT_FLOAT &&
-               value_shape.dims() == 1) {
+          absl::MakeConstSpan(value_pointer + num_elements * outer_dim,
+                              value_pointer + num_elements * (outer_dim + 1))));
+    } else if (kind == "buffer" && value_dtype == tensorflow::DT_FLOAT) {
+      const int32 buffer_length = value_shape.dim_size(value_shape.dims() - 1);
+
+      const auto value_pointer = value.flat<float>().data();
       TF_RETURN_IF_ERROR(rasterizer->SetShaderStorageBuffer(
-          name, absl::MakeConstSpan(value.flat<float>())));
+          name, absl::MakeConstSpan(
+                    value_pointer + buffer_length * outer_dim,
+                    value_pointer + buffer_length * (outer_dim + 1))));
     } else {
       return tensorflow::errors::InvalidArgument(
           "Don't know how to handle variable with name='", name,
           "', kind=", kind, " shape=", value_shape.DebugString(),
           " and type=", value_dtype);
+    }
+  }
+  return tensorflow::Status::OK();
+}
+
+tensorflow::Status RasterizeOp::ValidateVariables(
+    tensorflow::OpKernelContext* context,
+    tensorflow::TensorShape* batch_shape) {
+  tensorflow::OpInputList variable_values;
+  TF_RETURN_IF_ERROR(context->input_list("variable_values", &variable_values));
+
+  if (variable_names_.size() != variable_values.size() ||
+      variable_names_.size() != variable_kinds_.size()) {
+    return tensorflow::errors::InvalidArgument(
+        "The variable names, kinds, and values must have the same size.");
+  }
+
+  bool batch_initialized = false;
+  batch_shape->Clear();
+
+  for (int index = 0; index < variable_kinds_.size(); ++index) {
+    const std::string name = variable_names_[index];
+    const std::string kind = variable_kinds_[index];
+    const tensorflow::Tensor& value = variable_values[index];
+    const tensorflow::DataType value_dtype = value.dtype();
+    tensorflow::TensorShape value_batch_shape = value.shape();
+
+    if (kind == "mat" && value_dtype == tensorflow::DT_FLOAT) {
+      value_batch_shape.RemoveLastDims(2);
+    } else if (kind == "buffer" && value_dtype == tensorflow::DT_FLOAT) {
+      value_batch_shape.RemoveLastDims(1);
+    } else {
+      return tensorflow::errors::InvalidArgument(
+          "Don't know how to handle variable with name='", name,
+          "', kind=", kind, " and type=", value_dtype);
+    }
+    if (batch_initialized == false) {
+      *batch_shape = value_batch_shape;
+      batch_initialized = true;
+    } else if (*batch_shape != value_batch_shape) {
+      return tensorflow::errors::InvalidArgument(
+          "Incompatible batch shape for variable with name='", name,
+          "', batch shape=", value_batch_shape);
     }
   }
   return tensorflow::Status::OK();
